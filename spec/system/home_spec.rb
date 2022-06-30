fa71c25d29cc41e91631164540a7d217fe396894
ucdj-marvel <ucdj.marvel@gmail.com>
2022-06-30 18:02:40 +0900

    #10 認証周りのE2Eテスト追加

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,19 +1,45 @@
 require "rails_helper"
 
 describe "Home", type: :system do
+  let!(:test_user) { create(:user) }
+
   before do
     driven_by(:chrome)
+    visit root_path
   end
 
   it "トップページで'MEN RANKING'が表示されること" do
-    visit root_path
     expect(current_path).to eq "/"
     expect(page).to have_content("MEN RANKING")
   end
 
-  it "サインアップする" do
-    visit root_path
+  it "ユーザー登録できること" do
     click_link("Sign-up")
-    sleep 5
+    fill_in "Username", with: "signup_test"
+    fill_in "Email", with: "signup_test@gmail.com"
+    fill_in "Password", with: "passpass"
+    fill_in "Password confirmation", with: "passpass"
+    click_on "Sign up"
+    expect(current_path).to eq "/"
+    expect(page).to have_content("Logout")
+  end
+
+  it "ログインできること" do
+    click_button("Login")
+    fill_in "Email", with: test_user.email
+    fill_in "Password", with: test_user.password
+    click_button "Log in"
+    expect(current_path).to eq "/"
+    expect(page).to have_content("Logout")
+  end
+
+  it "ログアウトできること" do
+    click_button("Login")
+    fill_in "Email", with: test_user.email
+    fill_in "Password", with: test_user.password
+    click_button "Log in"
+    click_button "Logout"
+    expect(current_path).to eq "/"
+    expect(page).to have_content("Login")
   end
 end
